0e2075d6b0a1616a0a611d86bd5fb33e168d2702
abrielkarisma <abrielcha.ac@gmail.com>
2022-11-09 21:12:52 +0700

    update abriel

---
+++ js/People.js
@@ -0,0 +1,12 @@
+class people{
+    constructor(username,name,email,password,phone){
+        this.username=username;
+        this.name=name;
+        this.email=email;
+        this.password=password;
+        this.phone=phone;
+    }
+    login(){
+        console.log ('login')
+    }
+}
---
+++ js/User.js
@@ -0,0 +1,26 @@
+class User extends People {
+    constructor(username,name,email,password,phone){
+        super(username,name,email,password,phone);
+    }
+    Register (){
+        console.log ('Anda Telah Terdaftar')
+    }
+    
+    Login (){
+        console.log ('Login Berhasil')
+    }
+    ReadNews (){
+        console.log ('Berita Dibaca')
+    }
+    RegisterConsult (){
+        console.log ('Daftar Konsultasi')
+    }
+    Chat (){
+        console.log ('Chat Dimulai')
+    }
+    Calculate (){
+        console.log ('Makanan Dihitung')
+    }
+
+    
+}
