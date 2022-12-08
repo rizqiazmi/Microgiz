26afb289a83e7ce0e92d5833b18cbad31bf3f754
Nuha Alfarhani <muhammad.difa.21@student.se.ittelkom-sby.ac.id>
2022-12-08 11:33:14 +0700

    NH

---
+++ php/consultOnline.php
@@ -0,0 +1,31 @@
+<?php
+
+$name = $_POST['namaLengkap'];
+echo "<pre>";
+var_dump($name);
+echo "</pre>";
+
+$age = $_POST['umur'];
+echo "<pre>";
+var_dump($age);
+echo "</pre>";
+
+$sex = $_POST['jenisKelamin'];
+echo "<pre>";
+var_dump($sex);
+echo "</pre>";
+
+$schedule = $_POST['jadwal'];
+echo "<pre>";
+var_dump($schedule);
+echo "</pre>";
+
+$payments = $_POST['pembayaran'];
+echo "<pre>";
+var_dump($name);
+echo "</pre>";
+
+$accNumb = $_POST['nomorRekening'];
+echo "<pre>";
+var_dump($name);
+echo "</pre>";
