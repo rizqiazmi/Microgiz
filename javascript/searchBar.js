a51f68297fc71b11022a3b0dab514808eec3f85e
abrielkarisma <abrielcha.ac@gmail.com>
2022-12-15 10:39:52 +0700

    update

--- javascript/searchBar.js
+++
@@ -1,6 +0,0 @@
-let inputBox = document.querySelector(".input-box"),
-                searchIcon = document.querySelector(".icon"),
-                closeIcon = document.querySelector(".close-icon");
-
-            searchIcon.addEventListener("click", () => inputBox.classList.add("open"));
-            closeIcon.addEventListener("click", () => inputBox.classList.remove("open"));
