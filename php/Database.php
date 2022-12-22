f4a77f14026db7f2f469e8d101ca84e1377eac38
Nuha Alfarhani <muhammad.difa.21@student.se.ittelkom-sby.ac.id>
2022-12-22 11:48:12 +0700

    N

--- php/Database.php
+++ php/Database.php
@@ -1,10 +1,10 @@
 <?php
 class Database{
     public $koneksi;
-    public $host;
-    public $dbname;
-    public $dbuser;
-    public $dbpass;
+    public $host = "localhost";
+    public $dbname = "microgiz";
+    public $dbuser = "root";
+    public $dbpass = "";
     public $db;
 
     function __construct($host,$dbname,$dbuser,$dbpass)
@@ -12,12 +12,6 @@ class Database{
         $this->koneksi="mysql:host=".$host.";dbname=".$dbname.";charsetutf8";
         $this->db = new PDO($this->koneksi,$dbuser,$dbpass);
         $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    }
-
-    function register(){ // cek
-        echo "User:".$this->user;
-        echo "</br>";
-        echo "Password:".$this->password;
     }
 }
 ?>
