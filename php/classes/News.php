9376ab6351fc94c0ba15eee572201891814be2e0
abrielkarisma <abrielcha.ac@gmail.com>
2022-12-22 10:48:32 +0700

    ppp

---
+++ php/classes/News.php
@@ -0,0 +1,23 @@
+<?php
+class User{
+    public $id;
+    public $title;
+    public $body;
+    public $author;
+    public $date;
+    public $tag;
+
+    
+    function addNews(){
+        echo "added";
+    }
+    function readNews(){
+        echo "readed";
+    }
+    function updateNews(){
+        echo "updated";
+    }
+    function deleteNews(){
+        echo "deleted";
+    }
+
