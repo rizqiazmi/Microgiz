3034e3c7169d34a1a76cf423adb74b9f959e5401
M Rizqi Azmi <58349611+rizqiazmi@users.noreply.github.com>
2022-12-22 11:41:12 +0700

    Update Database.php

--- php/Database.php
+++ php/Database.php
@@ -1,1 +1,23 @@
-<?PHP
+<?php
+class Database{
+    public $koneksi;
+    public $host;
+    public $dbname;
+    public $dbuser;
+    public $dbpass;
+    public $db;
+
+    function __construct($host,$dbname,$dbuser,$dbpass)
+    {
+        $this->koneksi="mysql:host=".$host.";dbname=".$dbname.";charsetutf8";
+        $this->db = new PDO($this->koneksi,$dbuser,$dbpass);
+        $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+
+    function register(){ // cek
+        echo "User:".$this->user;
+        echo "</br>";
+        echo "Password:".$this->password;
+    }
+}
+?>
