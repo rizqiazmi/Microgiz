f65da582fcf423d56c65f3563c41a30fc0ac5aee
M Rizqi Azmi <58349611+rizqiazmi@users.noreply.github.com>
2022-12-22 11:32:18 +0700

    Update Kalori.php

---
+++ php/classes/Kalori.php
@@ -0,0 +1,31 @@
+<?php
+class Kalori{
+
+    public $protein;
+    public $TotalLemak;
+    public $kalori;
+    public $glukosa;
+    public $natrium;
+
+    function __construct($prote, $Total, $kalori, $glukosa, $natrium)
+    {
+        $this->protein = $protein;
+        $this->TotalLemak = $TotalLemak;
+        $this->kalori = $kalori;
+        $this->glukosa = $glukosa;
+        $this->natrium = $natrium;
+    }
+
+    function serach(){
+        echo "Mencari Halaman Kalori";
+    }
+
+    function createUser(){ // boleh menggunakan array
+        $database = new Database("localhost","myweb","root","");
+        $query = "INSERT INTO `user` (`id`, `username`, `password`) VALUES (NULL, ?, ?)"; // sql
+        $statement = $database->db->prepare($query);
+        $parameters = [$this->user,$this->password]; //array parameter variabel
+        return $statement->execute($parameters);
+    }
+}
+?>
